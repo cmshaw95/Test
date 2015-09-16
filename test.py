6e0557ba2bba6c61ad52051ffff22ab62619e6ea
cmshaw95 <cmshaw95@live.com>
2015-09-16 11:24:32 -0400

    First Commit
    first commit

---
+++ test.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hello world"
+    
+@app.route("/colin")
+def var():
+	return render_template('/Users/colins/Documents/Test/template/test.html') 
+
+if __name__ == "__main__":
+    app.run(debug = True)
+    
+    
+    
